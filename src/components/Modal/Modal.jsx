aff32ca932bf257a8adb8fc654a7b7b23898e44b
ASyrotenko <andreysyrotenko@gmail.com>
2023-01-26 15:30:22 +0200

    translate Searchbar into hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,73 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import css from './modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-    document.querySelector('body').classList.add('modalOpen');
-  }
+export default function Modal({ url, closeModal, alt }) {
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    // document.querySelector('body').classList.add('modalOpen');
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      // document.querySelector('body').classList.remove('modalOpen');
+    };
+  }, [closeModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-    document.querySelector('body').classList.remove('modalOpen');
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.closeModal();
-    }
-  };
-
-  handleBackDropClick = e => {
+  const handleBackDropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.closeModal();
+      closeModal();
     }
   };
 
-  render() {
-    return (
-      <div className={css.Overlay} onClick={this.handleBackDropClick}>
-        <div className={css.Modal}>
-          <img src={this.props.url} alt={this.props.alt} />
-        </div>
+  return (
+    <div className={css.Overlay} onClick={handleBackDropClick}>
+      <div className={css.Modal}>
+        <img src={url} alt={alt} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default Modal;
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//     document.querySelector('body').classList.add('modalOpen');
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//     document.querySelector('body').classList.remove('modalOpen');
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   handleBackDropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.closeModal();
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className={css.Overlay} onClick={this.handleBackDropClick}>
+//         <div className={css.Modal}>
+//           <img src={this.props.url} alt={this.props.alt} />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Modal;
 
 Modal.propTypes = {
   url: PropTypes.string.isRequired,
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PropTypes from 'prop-types';
@@ -6,8 +6,8 @@ import PropTypes from 'prop-types';
 import css from './searchbar.module.css';
 import { ReactComponent as Search } from '../icons/search.svg';
 
-class Searchbar extends Component {
-  handleSubmit = e => {
+export default function Searchbar({ onSubmit }) {
+  const handleSubmit = e => {
     e.preventDefault();
     const { value } = e.currentTarget.elements.query;
     if (value.trim() === '') {
@@ -15,35 +15,66 @@ class Searchbar extends Component {
       return;
     }
 
-    this.props.onSubmit(value);
+    onSubmit(value);
     e.currentTarget.elements.query.value = '';
   };
-
-  render() {
-    return (
-      <header className={css.SearchBar}>
-        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.SearchFormButton}>
-            <Search width="25" />
-            <span className={css.SearchFormButtonLabel}>Search</span>
-          </button>
-
-          <input
-            className={css.SearchFormInput}
-            name="query"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.onChange}
-          />
-        </form>
-      </header>
-    );
-  }
+  return (
+    <header className={css.SearchBar}>
+      <form className={css.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={css.SearchFormButton}>
+          <Search width="25" />
+          <span className={css.SearchFormButtonLabel}>Search</span>
+        </button>
+        <input
+          className={css.SearchFormInput}
+          name="query"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
 }
 
-export default Searchbar;
+// class Searchbar extends Component {
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { value } = e.currentTarget.elements.query;
+//     if (value.trim() === '') {
+//       toast.error('Enter something to find.');
+//       return;
+//     }
+
+//     this.props.onSubmit(value);
+//     e.currentTarget.elements.query.value = '';
+//   };
+
+//   render() {
+//     return (
+//       <header className={css.SearchBar}>
+//         <form className={css.SearchForm} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={css.SearchFormButton}>
+//             <Search width="25" />
+//             <span className={css.SearchFormButtonLabel}>Search</span>
+//           </button>
+
+//           <input
+//             className={css.SearchFormInput}
+//             name="query"
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// export default Searchbar;
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
